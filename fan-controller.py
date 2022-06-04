dc06f747e22f1d55196fc54720c8d81f1eebf3c6
Jeremy Sapsed <dev@jsapsed.co.uk>
2022-06-04 15:51:58 +0100

    Changed fan curve

--- fan-controller.py
+++ fan-controller.py
@@ -4,7 +4,7 @@ import sys
 import time
 import RPi.GPIO as GPIO
 
-FAN_CURVE = [[40,0], [50,20], [60,50], [70,100]]
+FAN_CURVE = [[40,0], [50,20], [60,50], [70,80]]
 
 class RollingAverage:
     def __init__(self, size):
